f23fc77a17e9d7349e0bcf140108054ea50cd9da
jayedbinnazir <jayed.freelance@gmail.com>
2024-09-29 15:31:47 +0600

    nothing

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -64,7 +64,8 @@ export const checkAuth = createAsyncThunk(
       {
         withCredentials: true,
         headers: {
-          "content-type": "application/json",
+          "Cache-Control":
+            "no-store, no-cache, must-revalidate, proxy-revalidate",
         },
       }
     );
